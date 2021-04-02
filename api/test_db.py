b942ca12f32054b71c30d2093eb09d649adf1cea
D.J. Denton <ddenton@uoguelph.ca>
2021-04-02 14:48:47 -0400

    Remove old function.

--- api/test_db.py
+++ api/test_db.py
@@ -39,41 +39,6 @@ def create_table(conn, create_table_sql):
     except Error as e:
         print(e)
 
-def db_test():
-    # create a database connection
-    database = r"C:\sqlite\db\pythonsqlite.db"
-    conn = create_connection(database)
-
-    sql_create_projects_table = """ CREATE TABLE IF NOT EXISTS projects (
-                                        id integer PRIMARY KEY,
-                                        name text NOT NULL,
-                                        begin_date text,
-                                        end_date text
-                                    ); """
-
-    sql_create_tasks_table = """CREATE TABLE IF NOT EXISTS tasks (
-                                    id integer PRIMARY KEY,
-                                    name text NOT NULL,
-                                    priority integer,
-                                    status_id integer NOT NULL,
-                                    project_id integer NOT NULL,
-                                    begin_date text NOT NULL,
-                                    end_date text NOT NULL,
-                                    FOREIGN KEY (project_id) REFERENCES projects (id)
-                                );"""
-
-    # create tables
-    if conn is not None:
-        # create projects table
-        create_table(conn, sql_create_projects_table)
-
-        # create tasks table
-        create_table(conn, sql_create_tasks_table)
-
-        conn.close()
-    else:
-        print("Error! cannot create the database connection.")
-
 def insert_product(conn, product):
     sql = ''' INSERT INTO products(commonName,brand,fullName,price,store,tags)
               VALUES(?,?,?,?,?,?) '''
